965a161f02815f6783091e739d1901b89645bdae
Admin-github123 <patelsiddhant9297@gmail.com>
2024-06-24 13:49:41 +0530

    Employee cHANGE

--- src/p1/collectionExample/Employee.java
+++ src/p1/collectionExample/Employee.java
@@ -35,12 +35,5 @@ public class Employee {
         this.salary = salary;
     }
 
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
+
 }
